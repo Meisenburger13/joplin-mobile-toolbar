6eda2736dae1383191db302be4ed1743640842bd
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-03-16 19:15:52 +0100

    add characters to toolbar

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import joplin from 'api';
+import { ToolbarButtonLocation } from 'api/types';
 
 joplin.plugins.register({
 	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		await joplin.commands.register({
+			name: "insertEmdash",
+			label: "Insert emdash —",
+			iconName: "fas fa-minus",
+			execute: async () => {
+				await joplin.commands.execute("insertText", `—`);
+			}
+		});
+		await joplin.commands.register({
+			name: "insertRightArrow",
+			label: "Insert right arrow →",
+			iconName: "fas fa-arrow-right",
+			execute: async () => {
+				await joplin.commands.execute("insertText", `→`);
+			}
+		});
+		await joplin.views.toolbarButtons.create(
+			"insertEmdashButton",
+			"insertEmdash",
+			ToolbarButtonLocation.EditorToolbar
+		);
+		await joplin.views.toolbarButtons.create(
+			"insertRightArrowButton",
+			"insertRightArrow",
+			ToolbarButtonLocation.EditorToolbar
+		);
 	},
 });
